a61dedd06eac85bbf098d85206b646c5df04915d
Haopeng <haliu@zendesk.com>
2018-07-24 23:55:29 -0700

    3. Longest Substring Without Repeating Characters

---
+++ 3_Longest_Substring_Without_Repeating_Characters.rb
@@ -0,0 +1,22 @@
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+  return 0 if s.length==0
+  recorder = Hash.new
+  size = 0
+  (0..s.length-1).each do |i|
+    if recorder.include?(s[i])
+      size = recorder.size if size < recorder.size
+      index = recorder[s[i]]
+      recorder.delete_if {|k, v| v < index}
+    end
+    recorder[s[i]] = i
+  end
+  size = recorder.size if size < recorder.size
+  size
+end
+
+# test
+#
+s = "abcabcbb"
+puts length_of_longest_substring(s)
