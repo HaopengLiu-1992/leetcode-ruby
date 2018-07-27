081559ed09af3a5fb8d8c99411f5a9186c6150c1
Haopeng <haliu@zendesk.com>
2018-07-26 23:57:48 -0700

    Add a class

--- leetcode/3_Longest_Substring_Without_Repeating_Characters.rb
+++ leetcode/3_Longest_Substring_Without_Repeating_Characters.rb
@@ -1,17 +1,19 @@
 # @param {String} s
 # @return {Integer}
-def length_of_longest_substring(s)
-  return 0 if s.length==0
-  recorder = Hash.new
-  size = 0
-  (0..s.length-1).each do |i|
-    if recorder.include?(s[i])
-      size = recorder.size if size < recorder.size
-      index = recorder[s[i]]
-      recorder.delete_if {|k, v| v < index}
+class LengthOfLongestSubString
+  def solution(s)
+    return 0 if s.length==0
+    recorder = Hash.new
+    size = 0
+    (0..s.length-1).each do |i|
+      if recorder.include?(s[i])
+        size = recorder.size if size < recorder.size
+        index = recorder[s[i]]
+        recorder.delete_if {|k, v| v < index}
+      end
+      recorder[s[i]] = i
     end
-    recorder[s[i]] = i
+    size = recorder.size if size < recorder.size
+    size
   end
-  size = recorder.size if size < recorder.size
-  size
 end
