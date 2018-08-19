f3b2b447310c5f8798703946321a2028e367948f
Haopeng <haliu@zendesk.com>
2018-08-18 23:55:59 -0700

    872. Leaf-Similar Trees

---
+++ leetcode/872_Leaf_Similar_Trees.rb
@@ -0,0 +1,17 @@
+require_relative 'helper'
+
+# Leetcode 872
+class LeafSimilarTrees
+  include Helper::LinkedListHelper
+  def leaf_similar(root1, root2)
+    get_leaves(root1) == get_leaves(root2)
+  end
+
+  def get_leaves(root)
+    return [] if root.nil?
+    return [root.val] if root.left.nil? && root.right.nil?
+    left = get_leaves_from_left_to_right(root.left)
+    right = get_leaves_from_left_to_right(root.right)
+    left + right
+  end
+end
