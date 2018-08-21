159a484f46cde5c3027218b1f0de5e5bae1761c6
Haopeng Liu <haliu@zendesk.com>
2018-08-21 00:08:57 -0700

    [Leetcode] 75. Sort Colors (#7)
    * 75. Sort Colors
    
    * update rubocop and rubocop fix

---
+++ leetcode/75_Sort_Colors.rb
@@ -0,0 +1,26 @@
+# Leetcode 75
+class SortColors
+  # @param {Integer[]} nums
+  # @return {Void} Do not return anything, modify nums in-place instead.
+  def sort_colors(nums)
+    p = 0
+    (0...nums.length).each do |i|
+      if (nums[i]).zero?
+        nums[i] = nums[p]
+        nums[p] = 0
+        p += 1
+      end
+    end
+    (p...nums.length).each do |i|
+      if nums[i] == 1
+        nums[i] = nums[p]
+        nums[p] = 1
+        p += 1
+      end
+    end
+  end
+end
+
+# test
+test SortColors.new
+puts test.sort_colors([2, 0, 2, 1, 1, 0])
