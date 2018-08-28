b4d10834c09eaff50efa876ff110587456baa1ed
Haopeng Liu <haliu@zendesk.com>
2018-08-27 22:25:15 -0700

    [Leetcode] 19. Remove Nth Node From End of List (#16)
    * [Leetcode] 19. Remove Nth Node From End of List

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 task default: :test
 
 task :test do
-  ruby 'test/1*.rb'
-  ruby 'test/2*.rb'
+  ruby 'test/1_*.rb'
+  ruby 'test/19_*.rb'
+  ruby 'test/2_*.rb'
 end
---
+++ test/19_Remove_Nth_Node_From_End_Of_List_Test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require_relative '../leetcode/helper'
+require_relative '../leetcode/19_Remove_Nth_Node_From_End_Of_List '
+class RemoveNthNodeFromEndOfListTest < Test::Unit::TestCase
+  include Helper::LinkedListHelper
+  def setup
+    @l1 = init_list_by_array([1, 2, 3, 4, 5])
+    @n = 1
+    @res = [1, 2, 3, 4]
+    @remove = RemoveNthNodeFromEndOfList.new
+  end
+
+  def test_solution
+    sol = @remove.remove_nth_from_end(@l1, @n)
+    assert_equal list_to_array(sol), @res
+  end
+end
