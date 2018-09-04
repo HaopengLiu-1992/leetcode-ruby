560d03550c625ee046885f6244033b9080288c10
Haopeng <haliu@zendesk.com>
2018-09-03 23:58:55 -0700

    [LeetCode] 860. Lemonade Change

--- Rakefile
+++ Rakefile
@@ -4,6 +4,7 @@ task :test do
   ruby 'test/1_*.rb'
   ruby 'test/19_*.rb'
   ruby 'test/2_*.rb'
+  ruby 'test/860_*.rb'
   ruby 'test/888_*.rb'
   ruby 'test/896_*.rb'
 end
---
+++ test/860_Lemonade_Change_Test .rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require_relative '../leetcode/860_Lemonade_Change '
+class LemonadeChangeTest < Test::Unit::TestCase
+  def setup
+    @bills = [5, 5, 5, 10, 20]
+    @change = LemonadeChange.new
+  end
+
+  def test_solution
+    check = @change.lemonade_change(@bills)
+    assert_true check
+  end
+end
