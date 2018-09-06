93508316bd87c95dbbf3ced1f2c84670e4e240db
Haopeng Liu <haliu@zendesk.com>
2018-09-06 00:00:07 -0700

    [Leetcode] 9. Palindrome Number (#22)

--- Rakefile
+++ Rakefile
@@ -4,6 +4,7 @@ task :test do
   ruby 'test/1_*.rb'
   ruby 'test/19_*.rb'
   ruby 'test/2_*.rb'
+  ruby 'test/9_*.rb'
   ruby 'test/20_*.rb'
   ruby 'test/860_*.rb'
   ruby 'test/888_*.rb'
---
+++ test/9_Palindrome_Number_Test .rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require_relative '../leetcode/9_Palindrome_Number '
+class PalindromeNumberTest < Test::Unit::TestCase
+  def setup
+    @n = 121
+    @check = PalindromeNumber.new
+  end
+
+  def test_solution
+    sol = @check.palindrome?(@n)
+    assert_true sol
+  end
+end
